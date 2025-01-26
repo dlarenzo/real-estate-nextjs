9c603db250396c2bbbefe79ddaea143647ece746
LaRenzo <dlarenzo@outlook.com>
2025-01-26 13:03:04 -0700

    updated mongoose js file

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -6,19 +6,19 @@ export const connect = async () => {
   mongoose.set("strictQuery", true);
 
   if (initialized) {
-    console.log("Database already initialized");
+    console.log("MongoDB already connected");
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "next-realtor",
-      userNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     initialized = true;
     console.log("MongoDB connected");
   } catch (error) {
-    console.log("MongoDB connection failed", error);
+    console.log("MongoDB connection error:", error);
   }
 };
