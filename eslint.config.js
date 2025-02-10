dde111bcdf03469a572535eeb9494d60ffd9e642
LaRenzo <dlarenzo@outlook.com>
2025-02-10 14:00:30 -0700

    eslint 4

--- eslint.config.js
+++ eslint.config.js
@@ -11,8 +11,10 @@ export default [
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
-      ecmaFeatures: {
-        jsx: true,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     plugins: {
