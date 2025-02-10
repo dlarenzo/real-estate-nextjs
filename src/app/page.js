90bf00b8cd52b4b46322f6d06281dde03c64799e
LaRenzo <dlarenzo@outlook.com>
2025-02-10 13:48:53 -0700

    eslint issues still going

--- src/app/page.js
+++ src/app/page.js
@@ -87,7 +87,8 @@ export default async function Home() {
               Listings
             </h2>
             <p className="text-3xl my-5">
-              Check out the listings you have added below!!!YAY CRYING NOW
+              Check out the listings you have added below!!!YAY CRYING NOW. NEW
+              FILE
             </p>
           </div>
           <div className="my-5 py-5">
