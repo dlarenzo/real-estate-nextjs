99e82cd86551bd9d2fe3f9dae65a840323fea8a8
LaRenzo <dlarenzo@outlook.com>
2025-02-10 13:32:12 -0700

    updated eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,30 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@next/next/recommended",
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
 
 export default eslintConfig;
