a124d06f70436dafa038c7662c4d3a1abb43a279
LaRenzo <dlarenzo@outlook.com>
2025-02-11 10:40:56 -0700

    removed search function not working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,33 +3,9 @@ import { FaSearch } from "react-icons/fa";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
+// import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Header() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // CREATE HOOK TO GET SEARCH PARAMS
-  useEffect(() => {
-    const urlParams = new URLSearchParams(searchParams);
-    const searchTermFromUrl = urlParams.get("searchTerm");
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, [searchParams]);
-
-  // CREATE FUNCTION TO HANDLE SEARCH
-  const handleSubmit = (e) => {
-    // prevent refreshing page
-    e.preventDefault();
-    const urlParams = new URLSearchParams(searchParams);
-    urlParams.set("searchTerm", searchTerm);
-    const searchQuery = urlParams.toString();
-    // push to new url
-    router.push(`/search?${searchQuery}`);
-  };
-
   return (
     <header className="bg-slate-200 shadow-md z-20 fixed w-full">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -40,22 +16,9 @@ export default function Header() {
             <span className="text-yellow-500">Realty</span>
           </h1>
         </Link>
-        {/* SEARCH */}
-        <form
-          className="bg-slate-100 p-3 rounded-lg flex items-center "
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64 text-black"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button>
-            <FaSearch className="h-4 w-4 text-slate-600" />
-          </button>
-        </form>
+
+        {/* SEARCH COMING SOON */}
+
         {/* NAVIGATION */}
         <ul className="flex gap-4">
           <Link href="/">
