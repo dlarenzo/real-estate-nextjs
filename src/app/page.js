854226158f52f3284f3136bfa1306e57d8bb55f9
LaRenzo <dlarenzo@outlook.com>
2025-02-11 09:39:33 -0700

    back on track 2

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default async function Home() {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Find your next <span className="text-slate-500">perfect</span> <br />
-          place with ease{" "}
+          place with ease!!!!!!!!!{" "}
         </h1>
         <div className="text gray">
           Home Realty is the best place to find your next home. <br />
